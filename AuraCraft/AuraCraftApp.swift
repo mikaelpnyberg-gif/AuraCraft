06d35ec9b390f09ee7871a4c9bc1bf319749547e
Mikael Nyberg <mikael.p.nyberg@gmail.com>
2026-05-20 10:04:28 +1200

    Initial Commit

---
+++ AuraCraft/AuraCraftApp.swift
@@ -0,0 +1,32 @@
+//
+//  AuraCraftApp.swift
+//  AuraCraft
+//
+//  Created by MNyberg on 20/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AuraCraftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AuraCraft/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AuraCraft
+//
+//  Created by MNyberg on 20/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
